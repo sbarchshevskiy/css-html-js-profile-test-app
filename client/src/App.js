8a098b3780652adbb64cd6a371d8aa2195dfe56e
sergey <sbarchshevskiy@gmail.com>
2021-06-29 10:20:27 -0400

    organize project structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Introduction from "./components/Introduction";
+import Projects from "./components/Projects";
+import Welcome from "./components/Welcome";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Contact/>
+      <Header/>
+      <Introduction/>
+      <Projects/>
+      <Welcome/>
     </div>
   );
 }
