dfaa8f6abc14caf27ae336f65b88ae4d3b920605
sergey <sbarchshevskiy@gmail.com>
2021-06-30 18:26:48 -0400

    create basic submit form for contact

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,23 @@
 import './Contact.scss';
 
 export default function Contact() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
   return(
     <div className="contact" id="contact">
-      <h1>Contact</h1>
+        <div className="left-part">
+          <img src="media/handshake.png" alt="contact"/>
+        </div>
+        <div className="right-part">
+          <h2>Contact</h2>
+          <form onSubmit={handleSubmit}>
+            <imput type="text" placeholder="Email"/>
+            <textarea placeholder="Enter your message"></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
     </div>
   )
 
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,18 +1,52 @@
 import './Projects.scss';
+import ProjectList from "./ProjectList";
+import {useState} from "react";
 
 export default function Projects() {
+
+  const [selected, setSelected] = useState('restaurant');
+
+  const list = [
+    {
+      id: "restaurant",
+      title: "Restaurant App"
+    },
+    {
+      id: "Orca",
+      title: "Orca Music Creation App"
+    },
+    {
+      id: "Url",
+      title: "URL Shortening App"
+    },
+    {
+      id: "Agenda",
+      title: "Agenda App"
+    },
+    {
+      id: "Ruby",
+      title: "Ruby on Rails Store App"
+    },
+    {
+      id: "Tweeter",
+      title: "Tweeter Social Media App"
+    }
+  ]
+
+
+
   return(
     <div className="projects" id="projects">
       <h1>Projects</h1>
-
         <ul>
-          <li className="active">Restaurant App</li>
-          <li>Music Creation App</li>
-          <li>URL Shortener</li>
-          <li>Agenda App</li>
-          <li>Ruby On Rails Store</li>
-          <li>Social Media App</li>
-
+          {list.map((item)=>{
+            return <ProjectList
+              title={item.title}
+              active={selected === item.id}
+              setSelected={setSelected}
+              id={item.id}
+            />
+          })}
         </ul>
         <div className="container">
           <div className="item">
