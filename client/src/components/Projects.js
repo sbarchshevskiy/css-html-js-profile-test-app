a6792cf5b12564d0f42483acb0d4df262b533cdc
sergey <sbarchshevskiy@gmail.com>
2021-06-30 14:53:30 -0400

    implement existing projects to portfolio

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -6,14 +6,65 @@ export default function Projects() {
       <h1>Projects</h1>
 
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li className="active">Restaurant App</li>
+          <li>Music Creation App</li>
+          <li>URL Shortener</li>
+          <li>Agenda App</li>
+          <li>Ruby On Rails Store</li>
+          <li>Social Media App</li>
 
         </ul>
+        <div className="container">
+          <div className="item">
+            <img
+              src="media/scheduler.png"
+              alt="scheduler-app"
+            />
+            <h3>Scheduler App</h3>
+          </div>
+
+          <div className="item">
+            <img
+              src="media/scheduler.png"
+              alt="scheduler-app"
+            />
+            <h3>Scheduler App</h3>
+          </div>
+
+          <div className="item">
+            <img
+              src="media/scheduler.png"
+              alt="scheduler-app"
+            />
+            <h3>Scheduler App</h3>
+          </div>
+
+          <div className="item">
+            <img
+              src="media/scheduler.png"
+              alt="scheduler-app"
+            />
+            <h3>Scheduler App</h3>
+          </div>
+
+          <div className="item">
+            <img
+              src="media/scheduler.png"
+              alt="scheduler-app"
+            />
+            <h3>Scheduler App</h3>
+          </div>
+
+          <div className="item">
+            <img
+              src="media/scheduler.png"
+              alt="scheduler-app"
+            />
+            <h3>Scheduler App</h3>
+          </div>
+
+
+        </div>
 
     </div>
   )
